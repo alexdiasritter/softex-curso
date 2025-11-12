c6ebb6bd2f6d7f4b1fc647678487e75cb8e49361
AlexRitter1 <ritter.alex@hotmail.com>
2025-11-12 20:36:08 -0300

    feat: utilizando forms do django para tornar o desenvolvimento mais ágil e seguro

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,30 +1,26 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages  # Add for user feedback
+from .forms import TarefaForm
 from .models import Tarefa
 
-
 def home(request):
-    tarefas = Tarefa.objects.all()
+    tarefas = Tarefa.objects.all().order_by('-criada_em')
+    
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    else:
+        form = TarefaForm()
+        
     context = {
-        'nome_usuario': 'alex',
-        'tecnologias': ['Python, Java, Django, Models'],
-        'tarefas': tarefas
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python', 'Django', 'Models', 'Forms'],
+        'tarefas': tarefas,
+        'form': form,
     }
     return render(request, 'home.html', context)
-
-
-
-
-
-
-
-'''
-def home(request: HttpRequest) -> HttpResponse:
-        
-    my_context: dict = {
-        "name": "Alex",
-        "tecnologias": ['Python', 'Java', 'C', 'Javascript', 'HTML']
-        }
-    
-    return render(request,'home.html', context=my_context)
-'''
