38ad60ebda322865bf8d4f1ecda0e220c1832ac8
AlexRitter1 <ritter.alex@hotmail.com>
2025-11-17 15:23:04 -0300

    feat: add delete e update

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect, render
-from django.contrib import messages  # Add for user feedback
+from django.shortcuts import redirect, render, get_object_or_404  
 from .forms import TarefaForm
 from .models import Tarefa
 
@@ -24,3 +23,34 @@ def home(request):
         'form': form,
     }
     return render(request, 'home.html', context)
+
+def concluir_tarefa(request, pk):
+    
+    # 1. Busca a tarefa pela 'pk' (ID) vinda da URL.
+    # Se não achar, retorna um erro 404.
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    
+    # 2. Segurança: Apenas execute se o método for POST
+    if request.method == 'POST':
+
+        # 3. A Lógica de "Update"
+        tarefa.concluida = True
+
+        tarefa.save() # Não se esqueça de salvar!
+
+        # 4. Redireciona de volta para a 'home' (Padrão PRG)
+    return redirect('home')
+
+def deletar_tarefa(request, pk):
+    
+    # 1. Busca a tarefa
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+
+    # 2. Segurança: Apenas execute se o método for POST
+    if request.method == 'POST':
+        
+        # 3. A Lógica de "Delete"
+        tarefa.delete()
+
+    # 4. Redireciona de volta para a 'home'
+    return redirect('home')
